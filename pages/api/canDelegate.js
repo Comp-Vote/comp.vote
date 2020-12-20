43320b512d7ce40ec59cc0d2887b7d0523b4af77
Aryeh Greenberg <13561405+arr00@users.noreply.github.com>
2020-12-19 20:34:37 -0500

    spelling and canDelegate fix

--- pages/api/canDelegate.js
+++ pages/api/canDelegate.js
@@ -1,11 +1,11 @@
 import { canDelegate } from "helpers"; // canDelegate helper
 
 export default async (req, res) => {
-  const { address } = req.query; // Collect address from request
+  const { address, delegatee } = req.query; // Collect address from request
 
   try {
     // Check if address can delegate
-    await canDelegate(address);
+    await canDelegate(address, delegatee);
   } catch (error) {
     // If error, return response
     res.status(error.code).send({
