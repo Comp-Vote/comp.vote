c32706e48fcb47ac449c5f272678805654ddffec
Arr00 <13561405+arr00@users.noreply.github.com>
2025-06-22 19:27:12 -0400

    Fix proposal 450 description (#39)

--- pages/api/governance/proposals.js
+++ pages/api/governance/proposals.js
@@ -24,6 +24,7 @@ const statesKey = [
 /// Global defining titles for misformatted proposals
 const MISFORMATTED_PROPOSAL_TITLES = {
   380: "[Gauntlet] Supply Cap Recommendations (12/09/24)",
+  450: "OpenZeppelin Security Partnership - Annual Renewal 2025",
 };
 
 const initialProposalBravo = 42;
